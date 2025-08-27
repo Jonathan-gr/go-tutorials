bc98093c69f693882b8ccfc2091db99bd7a0194d
Jonathan Grinshpan <jonathan.grinshpan@gmail.com>
2025-08-27 20:03:24 +0300

    basic example

--- tutorial3-channels/main.go
+++ tutorial3-channels/main.go
@@ -2,24 +2,8 @@ package main
 
 func main() {
 
-	//unbuffered channel
-	//causes a deadlock
-	// userch := make(chan string)
-
-	// userch <- "hello2"
-	// userch <- "hello"
-	// user := <-userch
-	// user2 := <-userch
-	// println(user)
-	// println(user2)
-
-	// buffered channel
-	userch := make(chan string, 2)
-	userch <- "hello2"
-	userch <- "hello"
-	user := <-userch
-	user2 := <-userch
-	println(user)
-	println(user2)
+	ch := make(chan int)
+	ch <- 42
+	println(<-ch)
 
 }
