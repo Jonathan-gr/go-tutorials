4b998da1543b728fcdf7338eaab8ad9dfdb9f553
Jonathan Grinshpan <jonathan.grinshpan@gmail.com>
2025-08-29 17:14:59 +0300

    without concurency

--- tutorial2-concurency/main.go
+++ tutorial2-concurency/main.go
@@ -1,7 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 
-	println("Concurency")
+	now := time.Now()
 
+	userID := 10
+
+	data := fetchUserData(userID)
+	rec := fetchUserRecomendations(userID)
+	likes := fetchUserLikes(userID)
+
+	fmt.Println(data)
+	fmt.Println(rec)
+	fmt.Println(likes)
+
+	fmt.Println(time.Since(now))
+
+}
+
+func fetchUserData(userID int) string {
+	time.Sleep(100 * time.Millisecond)
+	return "user data"
+}
+
+func fetchUserRecomendations(userID int) string {
+	time.Sleep(100 * time.Millisecond)
+	return "user recomendations"
+}
+
+func fetchUserLikes(userID int) string {
+	time.Sleep(100 * time.Millisecond)
+	return "user likes "
 }
