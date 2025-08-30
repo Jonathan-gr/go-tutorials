740ea3edd37d87f05cdd8f6dd8bec790541d68dd
Jonathan Grinshpan <jonathan.grinshpan@gmail.com>
2025-08-30 17:21:01 +0300

    race condition

--- tutorial2-concurency/main.go
+++ tutorial2-concurency/main.go
@@ -1,38 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"testing"
 )
 
-func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan string)
+func TestingRaceonditions(t *testing.T) {
+	var state int32
 
-	// Goroutine 1
-	go func() {
-		time.Sleep(1 * time.Second)
-		ch1 <- 21
-	}()
-
-	// Goroutine 2
-	go func() {
-		time.Sleep(2 * time.Second)
-		ch2 <- "message from ch2"
-	}()
-
-	// Loop that waits for messages
-	for {
-		select {
-		case msg1 := <-ch1:
-			fmt.Println("Received:", msg1)
-		case msg2 := <-ch2:
-			fmt.Println("Received:", msg2)
-			return // exit after receiving from ch2
-		default:
-			// runs immediately if no case is ready
-			fmt.Println("No messages yet...")
-			time.Sleep(300 * time.Millisecond)
-		}
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			state += int32(i)
+		}(i)
 	}
 }
