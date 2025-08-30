9c656a4fbe359c1fdf1f96dcbda4a3f7128e58b2
Jonathan Grinshpan <jonathan.grinshpan@gmail.com>
2025-08-30 17:39:33 +0300

    using mutex

---
+++ tutorial2-concurency/go.mod
@@ -0,0 +1,3 @@
+module tutorial2-concurency
+
+go 1.23.2
--- tutorial2-concurency/main.go
+++ tutorial2-concurency/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-	"testing"
+	"fmt"
+	"sync"
 )
 
-func TestingRaceonditions(t *testing.T) {
+func testingRaceConditions(wg *sync.WaitGroup) int32 {
 	var state int32
+	var mu sync.Mutex
+	wg.Add(10)
 
 	for i := 0; i < 10; i++ {
 		go func(i int) {
+			mu.Lock()
+			defer wg.Done()
 			state += int32(i)
+			mu.Unlock()
 		}(i)
 	}
+	wg.Wait()
+	return state
+}
+
+func main() {
+	wg := sync.WaitGroup{}
+	res := testingRaceConditions(&wg)
+
+	fmt.Println(res) // deterministic result
 }
